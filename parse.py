e750c79d7361017ca0d4e7994efd357513a35d4b
wolendranh <hulpa.yura@gmail.com>
2017-07-28 18:15:49 +0300

    added command like possibility to run script and gather posts into .md file

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 import re
 from datetime import datetime, date
 import calendar
+import argparse
+import time
 from pprint import pprint
 
 from markdown2 import Markdown
@@ -19,23 +21,29 @@ def get_submissions(score=None, ups=None, subreddit=None):
         if ups and submission.ups >= ups:
             yield submission
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start")
+    parser.add_argument("end")
+    parser.add_argument("ups")
+    args = parser.parse_args()
+    start_date, end_date = time.strptime(args.start, '%Y-%m-%d'), time.strptime(args.end, '%Y-%m-%d')
+
     reddit = praw.Reddit(**CREDENTIALS)
 
     python_subreddit = reddit.subreddit(SUBREDDIT)
 
-    start_date = date(2017, 5, 1)
-    end_date =  date(2017, 5, 30)
+    start_timestamp = time.mktime(start_date)
+    end_timestamp = time.mktime(end_date)
+    ups = int(args.ups)
 
-    start_timestamp = calendar.timegm(start_date.timetuple())
-    end_timestamp = calendar.timegm(end_date.timetuple())
     link_patterns=[(re.compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'),r'\1')]
     markdown=Markdown(extras=["link-patterns"],link_patterns=link_patterns)
 
     # write to markdown file with links embedded
     with open('links.md', 'w') as file:
         id = 1
-        for submission in get_submissions(ups=40, subreddit=python_subreddit):
+        for submission in get_submissions(ups=ups, subreddit=python_subreddit):
             file.write(
                 '  '.join(
                     [
--- settings.py
+++ settings.py
@@ -1,7 +1,19 @@
 
 SUBREDDIT = 'python'
 
+# credentials format
+"""
+CREDENTIALS = {
+ 'client_id': ',
+ 'client_secret': '',
+ 'password': '',
+ 'username' : '',
+ 'user_agent': ''
+
+}
+"""
+
 try:
-    from local_settings improt *
+    from local_settings import *
 except ImportError:
     pass
